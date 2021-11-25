8785cd1491404ec3a361413080816774f5b1fec0
wakuwinmail <wakuwinmail@yahoo.co.jp>
2021-11-25 17:01:50 +0900

    add:データ読み込み用関数の作成

--- load_data.rb
+++ load_data.rb
@@ -1,7 +1,14 @@
 # coding: utf-8
 def load_data(bow_fn, mat_fn)
-  wtoi
-  itow
-
+  wtoi = Hash.new
+  itow = Hash.new
+  ind = 0
+  File.open("bow.in", "r:utf-8") do |file|
+    file.each_line do |word|
+      wtoi[word.chomp] = ind
+      itow[ind] = word.chomp
+      ind = ind+1
+    end
+  end
   return [wtio, itow]
 end
